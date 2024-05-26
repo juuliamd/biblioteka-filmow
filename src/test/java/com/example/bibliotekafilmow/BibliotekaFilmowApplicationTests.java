2026d652259db2a44ff1ff00e2dbf35de2d54124
juuliamd <julia.dela@outlook.com>
2024-05-26 14:56:30 +0200

    idkdik

---
+++ src/test/java/com/example/bibliotekafilmow/BibliotekaFilmowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotekafilmow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekaFilmowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
