b7da4133015e8183e1939210d95a626fc38edbf4
juuliamd <julia.dela@outlook.com>
2024-05-26 20:48:35 +0200

    idkdik

--- src/main/java/com/example/bibliotekafilmow/model/ListyOgladania.java
+++ src/main/java/com/example/bibliotekafilmow/model/ListyOgladania.java
@@ -1,4 +1,12 @@
 package com.example.bibliotekafilmow.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class ListyOgladania {
+    @Id
+    @GeneratedValue
+    private Integer id;
 }
