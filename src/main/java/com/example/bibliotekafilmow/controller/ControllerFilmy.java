5aa5ab798739163e3c48913bd41252f91799d249
juuliamd <julia.dela@outlook.com>
2024-05-26 18:49:32 +0200

    idkdik

--- src/main/java/com/example/bibliotekafilmow/controller/ControllerFilmy.java
+++ src/main/java/com/example/bibliotekafilmow/controller/ControllerFilmy.java
@@ -5,7 +5,10 @@ import com.example.bibliotekafilmow.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,70 +24,76 @@ public class ControllerFilmy {
     }
 
     @GetMapping
-    public Page<Filmy> findAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
+    public ResponseEntity<Page<Filmy>> findAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
         PageRequest pr=PageRequest.of(page, size);
-        return filmRepository.findAll(pr);
+        Page<Filmy> filmsPage = filmRepository.findAll(pr);
+        return ResponseEntity.ok(filmsPage);
     }
 
     @PostMapping
-
-    //@GetMapping
-    public Filmy addFilm(@RequestBody Filmy filmy){
-        return filmRepository.save(filmy);
+    public ResponseEntity<Filmy> addFilm(@RequestBody Filmy filmy){
+        Filmy savedFilm=filmRepository.save(filmy);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedFilm);
     }
 
     //edycja filmu
     @PutMapping("/{id}")
-    public Filmy updateFilm(@PathVariable Integer id, @RequestBody Filmy updatedFilm){
+    public ResponseEntity<Filmy> updateFilm(@PathVariable Integer id, @RequestBody Filmy updatedFilm){
         Optional<Filmy> optionalFilm = filmRepository.findById(id);
         if(optionalFilm.isPresent()) {
             Filmy film = optionalFilm.get();
-            film.setTitle(updatedFilm.getTitle());
-            film.setReleaseYear(updatedFilm.getReleaseYear());
-            film.setGenre(updatedFilm.getGenre());
-            film.setWatched(updatedFilm.isWatched());
-            film.setRating(updatedFilm.getRating());
-            film.setReview(updatedFilm.getReview());
-            return filmRepository.save(film);
+                film.setTitle(updatedFilm.getTitle());
+                film.setReleaseYear(updatedFilm.getReleaseYear());
+                film.setGenre(updatedFilm.getGenre());
+                film.setWatched(updatedFilm.isWatched());
+                film.setRating(updatedFilm.getRating());
+                film.setReview(updatedFilm.getReview());
+                Filmy savedFilm= filmRepository.save(film);
+            return ResponseEntity.ok(savedFilm);
         }
         else{
-            throw new RuntimeException("No film with such ID: "+id);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"no film with such id: "+id);
         }
 
     }
     //usuwanie
     @DeleteMapping("/{id}")
-    public void deleteFilm(@PathVariable Integer id){
+    public ResponseEntity<Void> deleteFilm(@PathVariable Integer id){
         filmRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
     //wyszukiwanie po tytule
 
     @GetMapping("/search")
-    public List<Filmy> searchFilmsByTitle(@RequestParam String title){
-       return filmRepository.findByTitleContainingIgnoreCase(title);
+    public ResponseEntity<List<Filmy>> searchFilmsByTitle(@RequestParam String title){
+        List<Filmy> foundFilms = filmRepository.findByTitleContainingIgnoreCase(title);
+        return ResponseEntity.ok(foundFilms);
     }
 
     //po gatunkach
     @GetMapping("/filter")
-    public List<Filmy> filterFilmsByGenre(@RequestParam String genre) {
-        return filmRepository.findByGenre(genre);
+    public ResponseEntity<List<Filmy>> filterFilmsByGenre(@RequestParam String genre) {
+        List<Filmy> filteredFilms = filmRepository.findByGenre(genre);
+        return ResponseEntity.ok(filteredFilms);
     }
 
     //sortowanie po ocenie
     @GetMapping("/sort")
-    public List<Filmy> sortFilmsByRating(@RequestParam String direction){
-        if(direction.equalsIgnoreCase("asc")){
-            return filmRepository.findAllByOrderByRatingAsc();
+    public ResponseEntity<List<Filmy>> sortFilmsByRating(@RequestParam String direction) {
+        List<Filmy> sortedFilms;
+        if (direction.equalsIgnoreCase("asc")) {
+            sortedFilms = filmRepository.findAllByOrderByRatingAsc();
+        } else {
+            sortedFilms = filmRepository.findAllByOrderByRatingDesc();
         }
-        else{
-            return filmRepository.findAllByOrderByRatingDesc();
-        }
+        return ResponseEntity.ok(sortedFilms);
     }
     //obejrzane/nieobejrzane
     @GetMapping("/watched")
-    public List<Filmy> filterByWatched(@RequestParam boolean watched){
-        return filmRepository.findByWatched(watched);
+    public ResponseEntity<List<Filmy>> filterByWatched(@RequestParam boolean watched) {
+        List<Filmy> filteredFilms = filmRepository.findByWatched(watched);
+        return ResponseEntity.ok(filteredFilms);
     }
 
 }
